d14b6e64002ef1bfc43ab04d9ca79eaf1e08c214
tao-wt <xx201890@126.com>
2017-06-10 14:03:17 +0800

    :modify_wt

--- Alert_ST.bat
+++ Alert_ST.bat
@@ -1,34 +1,36 @@
 @echo off
 
-title Alert-nagios
+title add Alert-nagios autoStart
 set n=0
 set sourceDir=%~dp0
 REM echo %sourceDir%
-reg query HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run | findstr /I /C:"Alert_ST.bat" >null || (
-	echo add start... 
-	reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /ve /t REG_SZ /d "%0"
-)
 
-where java >null
-if errorlevel 0 (
-	setlocal enabledelayedexpansion
-		for /f "tokens=1,2* delims=." %%a in ('where java') do (
-			set /a n+=1
-			set ijava_!n!=%%~dpa
-			REM set jjava_!n!=%%b
-		)
-		pushd "!ijava_1!" >null
-			REM echo 123
-			REM pause
-			REM set tjava=!ijava_1!.!jjava_1!
-			REM echo !ijava_1!
-			REM echo 
-			java -jar %sourceDir%\Alert_ST.jar 
-			REM pause
-		popd >null
-	endlocal
-	REM echo %tjava%
+reg query HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run | findstr /I /C:"Alert_ST" >null || (
+	echo add autoStart... 
+	where javaw >null
+	if errorlevel 0 (
+		setlocal enabledelayedexpansion
+			for /f "tokens=1,2* delims=." %%a in ('where javaw') do (
+				set /a n+=1
+				set ijava_!n!=%%~dpa
+				REM set jjava_!n!=%%b
+			)
+			reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v Alert_ST /t REG_SZ /d "!ijava_1! -jar %sourceDir%\Alert_ST.jar"
+			REM pushd "!ijava_1!" >null
+				REM pause
+				REM set tjava=!ijava_1!.!jjava_1!
+				REM echo !ijava_1!
+				REM java -jar %sourceDir%\Alert_ST.jar 
+				REM pause
+			REM popd >null
+		endlocal
+		REM echo %tjava%
+	) else goto javawError
 )
-
+echo *** add autoStart finishd! ***
 exit 0
 REM pause
+
+:javawError
+echo java maybe not install in your computer,please check.
+exit 3
