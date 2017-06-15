38056deabfcb59f817afdb94197994a0e7c6c7dc
tao-wt <xx201890@126.com>
2017-06-15 16:24:34 +0800

    Create Alert_ST.bat

--- Alert_ST.bat
+++ Alert_ST.bat
@@ -5,9 +5,9 @@ set n=0
 set sourceDir=%~dp0
 REM echo %sourceDir%
 
-reg query HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run | findstr /I /C:"Alert_ST" >null || (
+reg query HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run | findstr /I /C:"Alert_ST" >nul || (
 	echo add autoStart... 
-	REM where javaw >null 2>&1
+	REM where javaw >nul 2>&1
 	REM if errorlevel 0 (
 	where javaw >null 2>&1 && (
 		setlocal enabledelayedexpansion
@@ -17,28 +17,28 @@ reg query HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run | find
 				REM set jjava_!n!=%%b
 			)
 			reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v Alert_ST /t REG_SZ /d "!ijava_1! -jar %sourceDir%\Alert_ST.jar" || goto regError
-			REM pushd "!ijava_1!" >null
+			REM pushd "!ijava_1!" >nul
 				REM pause
 				REM set tjava=!ijava_1!.!jjava_1!
 				REM echo !ijava_1!
 				REM java -jar %sourceDir%\Alert_ST.jar 
 				REM pause
-			REM popd >null
+			REM popd >nul
 		endlocal
 		goto addOk
 		REM echo %tjava%
 	)
 	REM ) else goto javawError
-	where javaw >null 2>&1 || goto javawError
+	where javaw >nul 2>&1 || goto javawError
 )
 echo *** Alert_ST autostart had added! ***
-ping 127.0.0.1 -n 5 >null
+ping 127.0.0.1 -n 5 >nul
 exit 0
 
 
 :addOk
 echo *** add autoStart finishd! cmd window close after 5s. ***
-ping 127.0.0.1 -n 5 >null
+ping 127.0.0.1 -n 5 >nul
 exit 0
 
 :javawError
